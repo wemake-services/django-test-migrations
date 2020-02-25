4ea40993f6eb79c4150d84efc6236b4a1d2e53ee
sobolevn <mail@sobolevn.me>
2020-02-25 17:19:03 +0300

    More docs

--- django_test_migrations/contrib/django_checks.py
+++ django_test_migrations/contrib/django_checks.py
@@ -10,11 +10,34 @@ class AutoNames(AppConfig):
     """
     Class to install this check into ``INSTALLED_APPS`` in ``django``.
 
+    If you have migrations that cannot be renamed,
+    use ``DTM_IGNORED_MIGRATIONS`` setting in ``django.conf``
+    to ignore ones you have to deal with:
+
+    .. code:: python
+
+        # settings.py
+        DTM_IGNORED_MIGRATIONS = {
+            ('main_app', '0004_auto_20191119_2125'),
+            ('dependency_app', '0001_auto_20201110_2100'),
+        }
+
+    To run checks use:
+
+    .. code:: bash
+
+        python manage.py check --deploy --fail-level WARNING
+
+    It will return exit code ``1`` if any violations are found.
+    This can be easily added into your CI.
+
     See:
         https://docs.djangoproject.com/en/3.0/ref/applications/
+        https://twitter.com/AdamChainz/status/1231895529686208512
 
     """
 
+    #: Part of Django API.
     name = CHECK_NAME
 
     def ready(self):
