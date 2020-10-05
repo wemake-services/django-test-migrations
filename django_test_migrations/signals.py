5c39516d7d4928535cb500884752f7e694f23cca
Maik Hoepfel <m@maikhoepfel.de>
2020-10-05 15:08:39 +0200

    Don't assign the same list
    The previous assignment would've assigned the same `list` object, meaning that changing one would've changed the other. As @skarzi pointed out, that leads to hard to debug errors.
    
    Co-authored-by: Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>

--- django_test_migrations/signals.py
+++ django_test_migrations/signals.py
@@ -8,7 +8,8 @@ def mute_migrate_signals():
     """Context manager to mute migration-related signals."""
     pre_migrate_receivers = pre_migrate.receivers
     post_migrate_receivers = post_migrate.receivers
-    pre_migrate.receivers = post_migrate.receivers = []
+    pre_migrate.receivers = []
+    post_migrate.receivers = []
     yield pre_migrate_receivers, post_migrate_receivers
     pre_migrate.receivers = pre_migrate_receivers
     post_migrate.receivers = post_migrate_receivers
