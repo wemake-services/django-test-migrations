e26c8585fef9d6ec6cbe627f9e10547cd6688b8c
sobolevn <mail@sobolevn.me>
2021-02-12 15:26:26 +0300

    Fixes CI

--- tests/test_logic/test_datetime.py
+++ tests/test_logic/test_datetime.py
@@ -9,8 +9,14 @@ from django_test_migrations.logic.datetime import timedelta_to_miliseconds
     (datetime.timedelta(seconds=1), 1000),
     (datetime.timedelta(minutes=3), 3 * 60 * 1000),
     (datetime.timedelta(hours=2.6), 2.6 * 60 * 60 * 1000),
-    (datetime.timedelta(days=4), 4 * 24 * 60 * 60 * 1000),
-    (datetime.timedelta(minutes=7.4, seconds=47), 7.4 * 60 * 1000 + 47 * 1000),
+    (
+        datetime.timedelta(days=4),
+        4 * 24 * 60 * 60 * 1000,
+    ),
+    (
+        datetime.timedelta(minutes=7.4, seconds=47),
+        7.4 * 60 * 1000 + 47 * 1000,
+    ),
 ])
 def test_timedelta_to_miliseconds(timedelta, expected_result):
     """Ensure expected value is returned."""
