62c342ed098423ef9912b2f5a9b77492f978377d
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2023-03-31 12:27:34 +0200

    chore(dev): remove django < 3.2 specific code

--- django_test_migrations/sql.py
+++ django_test_migrations/sql.py
@@ -1,12 +1,8 @@
-from functools import partial
-from typing import Callable, Dict, List, Optional
+from typing import Optional
 
-import django
 from django.core.management.color import Style, no_style
-from django.db import connections, transaction
+from django.db import connections
 from typing_extensions import Final
-
-from django_test_migrations.types import AnyConnection
 
 DJANGO_MIGRATIONS_TABLE_NAME: Final = 'django_migrations'
 
@@ -35,7 +31,7 @@ def drop_models_tables(
                 *sql_drop_tables,
                 'SET FOREIGN_KEY_CHECKS = 1;',
             ]
-        get_execute_sql_flush_for(connection)(sql_drop_tables)
+        connection.ops.execute_sql_flush(sql_drop_tables)
 
 
 def flush_django_migrations_table(
@@ -44,88 +40,17 @@ def flush_django_migrations_table(
 ) -> None:
     """Flush `django_migrations` table.
 
-    Ensures compatibility with all supported Django versions.
     `django_migrations` is not "regular" Django model, so its not returned
     by ``ConnectionRouter.get_migratable_models`` which is used e.g. to
-    implement sequences reset in ``Django==1.11``.
+    implement sequences reset.
 
     """
-    style = style or no_style()
     connection = connections[database_name]
-    execute_sql_flush = get_execute_sql_flush_for(connection)
-    sql_flush = get_sql_flush_with_sequences_for(connection)
-    execute_sql_flush(
-        sql_flush(
-            style,
+    connection.ops.execute_sql_flush(
+        connection.ops.sql_flush(
+            style or no_style(),
             [DJANGO_MIGRATIONS_TABLE_NAME],
             allow_cascade=False,
+            reset_sequences=True,
         ),
     )
-
-
-def get_django_migrations_table_sequences(
-    connection: AnyConnection,
-) -> List[Dict[str, str]]:
-    """`django_migrations` table introspected sequences.
-
-    Returns properly inspected sequences when using ``Django>1.11``
-    and static sequence for `id` column otherwise.
-
-    """
-    if hasattr(connection.introspection, 'get_sequences'):  # noqa: WPS421
-        with connection.cursor() as cursor:
-            return connection.introspection.get_sequences(
-                cursor,
-                DJANGO_MIGRATIONS_TABLE_NAME,
-            )
-    # for ``Django==1.11`` only primary key sequence is returned
-    return [{'table': DJANGO_MIGRATIONS_TABLE_NAME, 'column': 'id'}]
-
-
-def get_sql_flush_with_sequences_for(
-    connection: AnyConnection,
-):
-    """Harmonizes ``sql_flush`` across Django versions."""
-    if django.VERSION >= (3, 1):
-        return partial(connection.ops.sql_flush, reset_sequences=True)
-    return partial(
-        connection.ops.sql_flush,
-        sequences=get_django_migrations_table_sequences(connection),
-    )
-
-
-def get_execute_sql_flush_for(
-    connection: AnyConnection,
-) -> Callable[[List[str]], None]:
-    """Return ``execute_sql_flush`` callable for given connection.
-
-    This function also harmonizes the signature of ``execute_sql_flush``
-    to match Django 3.1 with ``sql_list`` as the only argument.
-
-    """
-    if django.VERSION >= (3, 1):
-        return connection.ops.execute_sql_flush
-    if django.VERSION >= (2, 0):
-        return partial(connection.ops.execute_sql_flush, connection.alias)
-    return partial(execute_sql_flush, connection)
-
-
-def execute_sql_flush(
-    connection: AnyConnection,
-    sql_list: List[str],
-) -> None:  # pragma: no cover
-    """Execute a list of SQL statements to flush the database.
-
-    This function is copy of ``connection.ops.execute_sql_flush``
-    method from Django's source code: https://bit.ly/3doGMye
-    to make `django-test-migrations` compatible with ``Django==1.11``.
-    ``connection.ops.execute_sql_flush()`` was introduced in ``Django==2.0``.
-
-    """
-    with transaction.atomic(
-        using=str(connection.alias),
-        savepoint=connection.features.can_rollback_ddl,
-    ):
-        with connection.cursor() as cursor:
-            for sql in sql_list:
-                cursor.execute(sql)
--- tests/test_sql/test_flush_utils.py
+++ tests/test_sql/test_flush_utils.py
@@ -1,20 +1,7 @@
-from contextlib import contextmanager
-from functools import partial
-
-import django
 import pytest
 from django.core.management.color import Style
 
 from django_test_migrations import sql
-
-
-@contextmanager
-def simulate_django_version(version):
-    """Context manager that changes ``django.VERSION`` to test compatibility."""
-    current_django_version = django.VERSION
-    django.VERSION = version
-    yield version
-    django.VERSION = current_django_version
 
 
 @pytest.fixture()
@@ -27,119 +14,18 @@ def testing_connection_mock(mocker):
     return testing_connection_mock
 
 
-class TestFlushDjangoMigrationTable(object):
-    """Ensure ``connection.ops`` methods are called with expected args."""
+def test_flush_django_migration_table(mocker, testing_connection_mock):
+    """Ensure expected ``connection`` methods are called."""
+    style = Style()
 
-    _style = Style()
+    sql.flush_django_migrations_table('test', style)
 
-    @pytest.mark.skipif(
-        django.VERSION < (3, 1),
-        reason='requires `Django>=3.1`',
+    testing_connection_mock.ops.sql_flush.assert_called_once_with(
+        style,
+        [sql.DJANGO_MIGRATIONS_TABLE_NAME],
+        reset_sequences=True,
+        allow_cascade=False,
     )
-    def test_django31_calls(self, mocker, testing_connection_mock):
-        """Check that the calls are right on Django 3.1."""
-        sql.flush_django_migrations_table('test', self._style)
-        testing_connection_mock.ops.sql_flush.assert_called_once_with(
-            self._style,
-            [sql.DJANGO_MIGRATIONS_TABLE_NAME],
-            reset_sequences=True,
-            allow_cascade=False,
-        )
-        testing_connection_mock.ops.execute_sql_flush.assert_called_once_with(
-            mocker.ANY,
-        )
-
-    @pytest.mark.skipif(
-        django.VERSION < (2, 0) or django.VERSION >= (3, 1),
-        reason='requires `2.0<=Django<3.1`',
+    testing_connection_mock.ops.execute_sql_flush.assert_called_once_with(
+        mocker.ANY,
     )
-    def test_django20_calls(self, mocker, testing_connection_mock):
-        """Check that the calls are right on Django 2.0 - 3.0."""
-        sql.flush_django_migrations_table('test', self._style)
-        testing_connection_mock.ops.sql_flush.assert_called_once_with(
-            self._style,
-            [sql.DJANGO_MIGRATIONS_TABLE_NAME],
-            sequences=[],
-            allow_cascade=False,
-        )
-        testing_connection_mock.ops.execute_sql_flush.assert_called_once_with(
-            mocker.ANY,
-            mocker.ANY,
-        )
-
-    @pytest.mark.skipif(
-        django.VERSION >= (2, 0),
-        reason='requires `Django<2.0`',
-    )
-    def test_django1_11_calls(self, testing_connection_mock):
-        """Check that the calls are right on Django < 2.0."""
-        sql.flush_django_migrations_table('test', self._style)
-        testing_connection_mock.ops.sql_flush.assert_called_once_with(
-            self._style,
-            [sql.DJANGO_MIGRATIONS_TABLE_NAME],
-            sequences=[],
-            allow_cascade=False,
-        )
-
-
-class TestGetSqlFlushWithSequences(object):
-    """Ensure we call ``sql_flush`` rightly across Django versions."""
-
-    def test_for_django31(self, mocker):
-        """Ensure we call ``sql_flush`` with ``reset_sequences``."""
-        connection_mock = mocker.Mock()
-        connection_mock.ops.sql_flush = _fake_sql_flush
-        with simulate_django_version((3, 1, 'final', 0)):
-            sql_flush = sql.get_sql_flush_with_sequences_for(connection_mock)
-        assert sql_flush.func == _fake_sql_flush
-        assert sql_flush.keywords == {'reset_sequences': True}
-
-    def test_for_django22(self, mocker):
-        """Ensure we call ``sql_flush`` with the positional ``sequences``."""
-        connection_mock = mocker.MagicMock()
-        connection_mock.ops.sql_flush.return_value = _fake_sql_flush
-        connection_mock.introspection.get_sequences.return_value = []
-        with simulate_django_version((2, 0, 'final', 0)):
-            sql_flush = sql.get_sql_flush_with_sequences_for(connection_mock)
-        assert sql_flush.func == connection_mock.ops.sql_flush
-        assert sql_flush.keywords == {'sequences': []}
-
-
-class TestGetExecuteSqlFlush(object):
-    """Ensure we call ``execute_sql_flush`` rightly across Django versions."""
-
-    def test_for_django31(self, mocker):
-        """Ensure we are getting ``execute_sql_flush`` directly."""
-        connection_mock = mocker.Mock()
-        connection_mock.ops.execute_sql_flush = _fake_execute_sql_flush
-        with simulate_django_version((3, 1, 'final', 0)):
-            execute_sql_flush = sql.get_execute_sql_flush_for(connection_mock)
-        assert execute_sql_flush == _fake_execute_sql_flush
-
-    def test_for_django20(self, mocker):
-        """Ensure we call ``execute_sql_flush`` with the database name."""
-        connection_mock = mocker.Mock()
-        connection_mock.alias = 'test'
-        connection_mock.ops.execute_sql_flush = _fake_execute_sql_flush
-        with simulate_django_version((2, 0, 'final', 0)):
-            execute_sql_flush = sql.get_execute_sql_flush_for(connection_mock)
-        assert execute_sql_flush.func == _fake_execute_sql_flush
-        assert execute_sql_flush.args[0] == 'test'
-
-    def test_for_django1_11(self, mocker):
-        """Ensure custom function is returned."""
-        connection_mock = mocker.Mock()
-
-        with simulate_django_version((1, 11, 'final', 0)):
-            execute_sql_flush = sql.get_execute_sql_flush_for(connection_mock)
-        assert isinstance(execute_sql_flush, partial)
-        assert execute_sql_flush.func == sql.execute_sql_flush
-        assert execute_sql_flush.args[0] == connection_mock
-
-
-def _fake_execute_sql_flush(sql_list):
-    """Mock that does nothing."""
-
-
-def _fake_sql_flush():
-    """Mock that does nothing."""
