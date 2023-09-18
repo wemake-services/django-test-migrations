b0ad682f3f67517ebfff81295e9eaa0e7cc0df38
Nikita Sobolev <mail@sobolevn.me>
2023-09-18 13:21:56 +0300

    Update signals.py

--- django_test_migrations/signals.py
+++ django_test_migrations/signals.py
@@ -2,10 +2,15 @@ from contextlib import contextmanager
 from typing import Any, Iterator, List, Tuple
 
 from django.db.models.signals import post_migrate, pre_migrate
+from typing_extensions import TypeAlias
+
+_MutedSignals: TypeAlias = Iterator[
+    Tuple[List[Any], List[Any]],  # type: ignore[misc]
+]
 
 
 @contextmanager
-def mute_migrate_signals() -> Iterator[Tuple[List[Any], List[Any]]]:
+def mute_migrate_signals() -> MutedSignals:
     """Context manager to mute migration-related signals."""
     pre_migrate_receivers = pre_migrate.receivers
     post_migrate_receivers = post_migrate.receivers
